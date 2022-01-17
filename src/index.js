9bee4c7b0409cfa5876debfe3f55d81ada6a9960
Vadim Sedelkin <vadim@sedelkin.ru>
2022-01-18 00:49:20 +0300

    feat: implement function decode

--- src/index.js
+++ src/index.js
@@ -36,9 +36,36 @@ const MORSE_TABLE = {
     '----.':  '9',
     '-----':  '0',
 };
-
+const lenLetterCode = 10;
+const lenSymbolCode = 2;
 function decode(expr) {
-    // write your solution here
+    let len = expr.length;
+    let array = [];
+    let letter = "";
+    for(let i = 0; i < len; i+=lenLetterCode){
+        letter = "";
+        for(let j = 0; j < lenLetterCode; j++){
+            letter+=expr[i+j]
+        }
+        array.push(parseInt(letter) || 0);
+    }
+    array = array.map(item => {
+        let letter = ""
+        if(item === 0){
+            letter = " ";
+            return letter;
+        } 
+        item = item.toString();
+        for(let i = 0; i < item.length-1; i+=lenSymbolCode){
+            if(item[i] === "1" && item[i+1] === "0")
+                letter += ".";
+            else if(item[i] === "1" && item[i+1] === "1")
+                letter += "-";
+        }
+
+        return MORSE_TABLE[letter];
+    });
+    return array.join('');
 }
 
 module.exports = {
